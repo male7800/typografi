d952a2b9bd8f33f09e29e383d81e67ec89a8fc54
Malene Lamb <male7800@stud.kea.dk>
2022-11-06 13:56:46 +0100

    font sat ind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        dancingscript:"'Dancing Script', cursive",
+        literata:"'Literata', serif",
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
